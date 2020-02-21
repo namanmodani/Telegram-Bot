6c2b3e295f965038a030fe7f38513fc4e4496bea
diogoscf <diogo.scf2003@gmail.com>
2020-02-21 13:01:59 +0100

    Optimize code

--- index.js
+++ index.js
@@ -1,58 +1,62 @@
-const tesseract = require('tesseract.js');
-const TelBot = require('node-telegram-bot-api');
-const request = require('request-promise');
-const token = '';
-const bot = new TelBot(token, { polling: true });
+process.env.NTBA_FIX_319 = 1; // prevent deprecated warning on startup
+require("dotenv").config();
+
+const tesseract = require("tesseract.js");
+const TelBot = require("node-telegram-bot-api");
+const TOKEN = process.env.TOKEN;
+const bot = new TelBot(TOKEN, {
+  polling: true
+});
 // Variations of Nice
 const variations = [
-  'Nice',
-  'Nicee',
-  'Noice',
-  'ładny',
-  'niice',
-  'Obligatory Nice',
-  'Umma nice',
-  'hehe yeah boi',
-  'ooh ya',
-  '( ͡° ͜ʖ ͡°)',
-  'damn bruh',
-  'hehehe',
-  'I concur-',
-  'did I hear 69?!',
-  'ikr'
+  "Nice",
+  "Nicee",
+  "Noice",
+  "ładny",
+  "niice",
+  "Obligatory Nice",
+  "Umma nice",
+  "hehe yeah boi",
+  "ooh ya",
+  "( ͡° ͜ʖ ͡°)",
+  "damn bruh",
+  "hehehe",
+  "I concur-",
+  "did I hear 69?!",
+  "ikr"
 ];
 
+/**
+ * Responds to a message with a reply from variations
+ * @param {Object} message The message to reply to
+ */
+function executeSixNine(message) {
+  const randomNice = variations[Math.floor(Math.random() * variations.length)];
+  bot.sendMessage(message.chat.id, randomNice, {
+    reply_to_message_id: message.message_id
+  });
+}
+
 // On New Message
-bot.on('message', async function(msg) {
-  const newMsg = msg.text;
-  if (newMsg != undefined) {
+bot.on("message", (msg) => {
+  if (msg.text !== undefined) {
     // Send if text contains 69
-    if (newMsg.includes('69') || newMsg.includes('6 9')) {
+    if (msg.text.includes("69") || msg.text.includes("6 9")) {
       executeSixNine(msg);
       return;
     }
   }
-  if (msg.photo != undefined) {
-    var photo_path;
-    // gets path of photo so we can get the photo url
-    let url = "https://api.telegram.org/bot" + token + "/getFile?file_id=" + msg.photo[0].file_id;
-    photo_path =  JSON.parse(await request(url)).result.file_path
-    // should begin to get photo url
-    let photo_url = "https://api.telegram.org/file/bot" + token +"/" + photo_path
-
-    tesseract.recognize(
-      photo_url,
-      'eng'
-    ).then(({ data: { text } }) => {
-      if (text.includes('69')) {
-        executeSixNine(msg)
-      }
-    })
+  if (msg.photo !== undefined) {
+    bot.getFileLink(msg.photo[0].file_id).then((link) => {
+      tesseract.recognize(link, "eng").then(({
+        data: {
+          text
+        }
+      }) => {
+        if (text.includes("69") || text.includes("6 9")) {
+          executeSixNine(msg);
+        }
+      });
+    });
   }
-
 });
-
-function executeSixNine(last_message) {
-  const randomNice = variations[Math.floor(Math.random() * variations.length)];
-  bot.sendMessage(last_message.chat.id, randomNice, { reply_to_message_id: last_message.message_id});
-}
